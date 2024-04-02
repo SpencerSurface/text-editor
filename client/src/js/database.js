4f7740b50cb844abd1cb0f7868c4d66c1fd16c57
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-04-02 16:44:09 -0400

    implement putDb()

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,15 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Accept some content and add it to the database
+export const putDb = async (content) => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put(content);
+  const result = await request;
+  console.log("Data saved", request);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
