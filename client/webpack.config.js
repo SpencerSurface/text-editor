c6f0f83f27b49d9eebd54bf917bc0929a2056783
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-04-02 15:47:34 -0400

    add css loaders and babel to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,7 +23,20 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.m?js$/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/plugin-transform-runtime"]
+            }
+          }
+        }
       ],
     },
   };
