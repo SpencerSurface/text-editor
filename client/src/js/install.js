c6285071d4ad49a05dd0590f3580ceec5a38ca23
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-04-04 22:54:07 -0400

    add install.js event listeners

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,30 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// DONE: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Save the event to prompt the user later
+    window.deferredPrompt = event;
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// DONE: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    // Get the event from earlier and prompt the user
+    const promptEvent = window.deferredPrompt;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    if (!promptEvent) {
+        return;
+    }
+
+    promptEvent.prompt();
+
+    // Overwrite the event so the user is not prompted again
+    window.deferredPrompt = null;
+});
+
+// DONE: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log("Successfully installed!");
+    // Make sure the prompt is overwritten
+    window.deferredPrompt = null;
+});
